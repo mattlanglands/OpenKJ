cf6b8d5e1c66d088875e44deb0332c06d6d62455
Matt Langlands <matt@mattlanglands.com>
2026-03-23 21:16:44 +1000

    Fix Qt 6 regex and alignment QVariant usage

--- src/okarchive.cpp
+++ src/okarchive.cpp
@@ -22,6 +22,7 @@
 #include "okarchive.h"
 #include <QFile>
 #include <QBuffer>
+#include <QRegularExpression>
 #include <QTemporaryDir>
 #ifdef Q_OS_WIN
 #include <io.h>
@@ -322,9 +323,9 @@ zipEntries OkArchive::getZipContents()
     }
     m_logger->trace("{} Infozip output: \n{}",m_loggingPrefix, output);
 #if QT_VERSION < QT_VERSION_CHECK(5, 15, 0)
-    QStringList data = output.split(QRegExp("[\r\n]"),QString::SkipEmptyParts);
+    QStringList data = output.split(QRegularExpression("[\r\n]"), Qt::SkipEmptyParts);
 #else
-    QStringList data = output.split(QRegExp("[\r\n]"),Qt::SkipEmptyParts);
+    QStringList data = output.split(QRegularExpression("[\r\n]"), Qt::SkipEmptyParts);
 #endif
     int fnStart = 0;
     int listStart = 0;
@@ -427,5 +428,4 @@ bool OkArchive::zipIsValid()
         return true;
     }
 }
-
 
